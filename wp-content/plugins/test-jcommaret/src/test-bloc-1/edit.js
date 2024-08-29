2fb8a4852cc1b47b584e8458ee853a958021e79c
Jérôme Commaret <jcommaret@gmail.com>
2024-08-29 14:08:39 +0200

    fix that things

---
+++ wp-content/plugins/test-jcommaret/src/test-bloc-1/edit.js
@@ -0,0 +1,33 @@
+import { __ } from '@wordpress/i18n'
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor'
+
+export default function Edit() {
+	const blockProps = useBlockProps({ })
+	// Liste des blocs autorisés
+	const ALLOWED_BLOCKS = [ 'core/group', 'core/columns', 'core/column', 'core/heading', 'core/paragraph', 'core/buttons', 'core/button', 'core/image', 'test-jcommaret/button-with-arrow' ];
+
+	// Utilisation des fonctions core de wordpress 
+	const BASE_TEMPLATE = [	
+		[ 'core/columns', {}, [
+			[ 'core/column', {}, [
+				[ 'core/heading', { placeholder: __( 'Votre titre', 'test-jcommaret' ) } ],
+				[ 'core/paragraph', { placeholder: __( 'Votre contenu', 'test-jcommaret' ) } ],
+				[ 'core/buttons', {}, [
+					[ 'test-jcommaret/button-with-arrow', {} ],
+				]],
+			]],
+			[ 'core/column', {}, [
+				[ 'core/image', { placeholder: __( 'Votre image', 'test-jcommaret' ) } ]
+			]],
+		]],
+	];
+
+	return (
+		<section { ...blockProps }>
+			<InnerBlocks
+				template={ BASE_TEMPLATE }
+				allowedBlocks={ ALLOWED_BLOCKS }	
+			/>
+		</section>
+	)
+}
--- wp-content/plugins/test-jcommaret/test-jcommaret.php
+++ wp-content/plugins/test-jcommaret/test-jcommaret.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Test Jcommaret
- * Description:       Example block scaffolded with Create Block tool.
+ * Plugin Name:       Citeo Blocks
+ * Description:       Citeo Extension for bloc 1 & 2
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           citeo.test
+ * Author:            Jérôme Commaret
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       test-jcommaret
@@ -23,8 +23,28 @@ if ( ! defined( 'ABSPATH' ) ) {
  * through the block editor in the corresponding context.
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
- */
+*/
+
 function create_block_test_jcommaret_block_init() {
-	register_block_type( __DIR__ . '/build' );
+	register_block_type( __DIR__ . '/build/test-bloc-1' );
+    register_block_type( __DIR__ . '/build/button-with-arrow' );
 }
 add_action( 'init', 'create_block_test_jcommaret_block_init' );
+
+add_action('enqueue_block_assets', function (): void {
+    wp_enqueue_style('dashicons');
+});
+
+// White list block so users cannot break anything
+function citeo_allowed_block_types($allowed_block_types, $editor_context) {
+    return array(
+        'core/paragraph',
+        'core/heading',
+        'core/image',
+        'core/button',
+		'test-jcommaret/test-bloc-1'
+    );
+}
+
+// Add the filter
+add_filter('allowed_block_types_all', 'citeo_allowed_block_types', 10, 2);
