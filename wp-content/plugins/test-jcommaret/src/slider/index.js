8526cb43d90d1c6a493f2636350ca262c3de126f
Jérôme Commaret <jcommaret@gmail.com>
2024-09-02 12:56:26 +0200

    chore : comments

--- wp-content/plugins/test-jcommaret/src/slider/index.js
+++ wp-content/plugins/test-jcommaret/src/slider/index.js
@@ -1,14 +1,20 @@
+// Importing the necessary functions and components from WordPress
 import { registerBlockType } from '@wordpress/blocks';
+
+// Importing the Edit and Save components for the block
 import Edit from './edit';
-import Save from './save';
+import save from './save';
+// Importing metadata for the block from a JSON file
 import metadata from './block.json';
 
-import './style.scss';
+// Importing the styles for the block
+import "./style.scss";
 
-// Enregistrer le bloc
-registerBlockType(metadata.name, {
-    ...metadata,
-    edit: Edit,
-    save: Save,
-});
+// Registering a new block type with the specified metadata
+registerBlockType( metadata.name, {
+	// Specifying the edit component for the block
+	edit: Edit,
+	// Specifying the save component for the block
+	save,
+} );
 
