e2cc5b07474027721275bfd19f34dd8ec1c8aff7
Jérôme Commaret <jcommaret@gmail.com>
2025-02-19 23:37:40 +0100

    Remove everything but the extension

--- wp-content/upgrade/citeo-blocks-1/build/slider/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => '7a433296bd00a2696dbf');
--- wp-content/upgrade/citeo-blocks-1/build/slider/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c62b97c71276262b3706');
